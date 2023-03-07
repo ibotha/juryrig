4cb08e5e7faa254881bb0fc6ec34e53153eff5e3
Isard Botha <isard.botha@gmail.com>
2023-03-07 13:36:19 +0000

    Use winit to create a window and handle simple events.

--- example_app/main.rs
+++ example_app/main.rs
@@ -1,20 +1,47 @@
-// #![allow(
-//     dead_code,
-//     unused_variables,
-//     clippy::too_many_arguments,
-//     clippy::unnecessary_wraps
-// )]
+use log::{info, error};
+use winit::{event_loop::EventLoop, window::WindowBuilder, event::{Event, WindowEvent, DeviceEvent, ElementState, VirtualKeyCode}};
 
-// pub mod vulkan;
-// use vulkanalia::prelude::v1_0::*;
-
-// use winit::dpi::LogicalSize;
-// use winit::event::{Event, WindowEvent};
-// use winit::event_loop::{ControlFlow, EventLoop};
-// use winit::window::WindowBuilder;
-use khodol::init_window;
 fn main() {
     pretty_env_logger::init();
 
-    init_window("My Window");
+    info!("Logs initialised.");
+
+    let event_loop = EventLoop::new();
+
+    match WindowBuilder::new().with_title("Rara se window").build(&event_loop) {
+        Ok(window) => {
+            event_loop.run(move |event, _, control_flow| {
+
+                match event {
+                    Event::WindowEvent { window_id, event: WindowEvent::CloseRequested } => {
+                        if window.id() == window_id {
+                            control_flow.set_exit()
+                        }
+                    }
+                    Event::DeviceEvent { device_id: _device_id, event } => {
+                        match event {
+                            DeviceEvent::Key(input) => {
+                                // info!("Key Event {:?}", input.virtual_keycode.unwrap());
+                                if input.state == ElementState::Pressed && input.virtual_keycode == Some(VirtualKeyCode::Escape) {
+                                    control_flow.set_exit()
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                    Event::Resumed => {
+                        //TODO: Initialise graphics context
+                        info!("Ready to init graphics");
+                        control_flow.set_wait()
+                    }
+                    _ => {
+                        control_flow.set_wait()
+                    }
+                }
+            })
+        }
+        Err(e) => {
+            error!("Failed to initialise window. {}", e.to_string());
+        }
+    }
 }
